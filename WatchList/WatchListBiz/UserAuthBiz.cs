4bd45da8ae5b18d526c3fa4d3717bebad0b2a50d
ARUN KOCHUKADAVIL <arunkochukadavil10@gmail.com>
2019-08-03 01:29:37 +0530

    added verification method logic at business layer, token generation yet to be done

---
+++ WatchList/WatchListBiz/UserAuthBiz.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using WatchListDataLayer.Repository;
+using WatchListDTOs;
+
+namespace WatchListBiz
+{
+    public class UserAuthBiz
+    {
+        public static Result<string> VerifyUser(UserAuthData userAuthData)
+        {
+            var result = new UserAuthDataRepo().VerifyCredentials(userAuthData);
+            if (result.IsSucceed)
+            {
+                //TODO Generate JWT string
+                return new Result<string>()
+                {
+                    IsSucceed = true,
+                    Messages = new List<string> { "JWT string here" }
+                };
+            }
+            return new Result<string>() {
+                IsSucceed = false,
+                Messages = new List<string> { "Invalid Credentials" }
+            };
+        }     
+    }
+}
