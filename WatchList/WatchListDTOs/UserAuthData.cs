88afd03a303e916270f6c482730771be0f99abc6
ARUN KOCHUKADAVIL <arunkochukadavil10@gmail.com>
2019-08-03 01:28:29 +0530

    added UserAuthData for receiving credentials from client side

---
+++ WatchList/WatchListDTOs/UserAuthData.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+namespace WatchListDTOs
+{
+
+    public class UserAuthData
+    {
+        [JsonProperty(PropertyName = "uid")]
+        public string UID { get; set; }
+        [JsonProperty(PropertyName = "pass")]
+        public string Password { get; set; }
+    }
+
+}
