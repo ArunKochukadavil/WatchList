efd13b4d6ec8aeb315e1b06e98e81ecad2a300a1
ARUN KOCHUKADAVIL <arunkochukadavil10@gmail.com>
2019-08-03 01:23:17 +0530

    added one endpoint for getting watchlist for specific user

---
+++ WatchList/WatchList/Controllers/WatchDataController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Http;
+using WatchListBiz;
+using WatchListDTOs;
+
+namespace WatchList.Controllers
+{
+    [RoutePrefix("WatchData")]
+    public class WatchDataController : ApiController
+    {
+        [Route("Get")]
+        [HttpGet]
+        public IHttpActionResult GetWatchData(string uid)
+        {
+            try
+            {
+                return Ok(WatchData.GetWatchData(uid));
+            }
+            catch(Exception ex)
+            {
+                return Ok(new Result(){
+                    IsSucceed=false,
+                    Exception = ex
+                });
+            }
+        }
+    }
+}
