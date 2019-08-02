bbd170466a4523e4da9bc2e3ea2b57fd5be9ff18
ARUN KOCHUKADAVIL <arunkochukadavil10@gmail.com>
2019-08-03 01:33:42 +0530

    added Constants class which will contain, all constants used in the project, right now all UserAuthData, WatchData both tables column names are stored

---
+++ WatchList/WatchListDataLayer/Constants.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WatchListDataLayer
+{
+    public static class Constants
+    {
+        public static class WatchData
+        {
+            public static readonly string ID = "id";
+            public static readonly string UID = "uid";
+            public static readonly string Name = "Name";
+            public static readonly string Genre = "Genre";
+            public static readonly string Season = "Season";
+            public static readonly string TotalEpisodes = "TotalEpisodes";
+            public static readonly string EpisodesCompleted = "EpisodesCompleted";
+            public static readonly string Status = "Status";
+            public static readonly string CreationTime = "CreationTime";
+            public static readonly string ModificationTime = "ModificationTime";
+        } 
+
+        public static class UserAuthData
+        {
+            public static string UID = "uid";
+            public static string Password = "password";
+            public static string CreationTime = "creationTime";
+            public static string ModifiedTime = "modifiedTime";
+            public static string LastLoginTime = "lastLoginTime";
+            public static string IsLoggedIn = "isLoggedIn";
+            public static string IsAllowed = "IsAllowed";
+        }
+    }                             
+}
