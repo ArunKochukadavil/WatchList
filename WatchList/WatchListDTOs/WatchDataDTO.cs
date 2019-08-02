8ecefd1822dd31ac1cf748d0d84646658f3de7d4
ARUN KOCHUKADAVIL <arunkochukadavil10@gmail.com>
2019-08-03 01:23:56 +0530

    added a WatchListDTO for sending user specific watch data

---
+++ WatchList/WatchListDTOs/WatchDataDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace WatchListDTOs
+{
+    public class WatchDataDTO
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public byte Season { get; set; }
+        public int TotalEpisodes { get; set; }
+        public int EpisodesCompleted { get; set; }
+        public string Status { get; set; }
+    }
+}
