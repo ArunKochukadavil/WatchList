45b4c885fbe756ba534a0358f2006c72366a84ab
ARUN KOCHUKADAVIL <arunkochukadavil10@gmail.com>
2019-08-03 01:34:16 +0530

    added endpoint for user credential verfication

---
+++ WatchList/WatchList/Controllers/UserAuthController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.Http;
+using WatchListBiz;
+using WatchListDTOs;
+
+namespace WatchList.Controllers
+{
+    [RoutePrefix("Auth")]
+    public class UserAuthController : ApiController
+    {
+        [HttpPost]
+        [Route("Login")]
+        public IHttpActionResult UserLogin(UserAuthData userAuthData)
+        {
+            try
+            {
+                return Ok(UserAuthBiz.VerifyUser(userAuthData));
+            }
+            catch(Exception ex)
+            {
+                return Ok(new Result()
+                {
+                    IsSucceed=false,
+                    Exception = ex
+                });
+            }
+        }
+    }
+}
