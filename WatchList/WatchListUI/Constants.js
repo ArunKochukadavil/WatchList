d15c0a78d611afa299b422643e38d16d6e00d42e
ARUN KOCHUKADAVIL <arunkochukadavil10@gmail.com>
2019-08-15 20:03:07 +0530

    added more constants

--- WatchList/WatchListUI/Constants.js
+++ WatchList/WatchListUI/Constants.js
@@ -11,17 +11,34 @@ var WatchListConstants;
             Selector.loginErrorMessage = "#LoginErrorMessage";
             Selector.dashboardWatchListDataTable = "#table_data";
             Selector.loader = "#loader";
+            Selector.signOut = "#sign_out";
+            Selector.watchDataTable = "#watchDataTable";
+            Selector.watchDataTableBody = "#watchDataTableBody";
             return Selector;
         }());
         ControlConstants.Selector = Selector;
+        var LocalStorage = /** @class */ (function () {
+            function LocalStorage() {
+            }
+            LocalStorage.tokenKey = "watchListSecureToken";
+            return LocalStorage;
+        }());
+        ControlConstants.LocalStorage = LocalStorage;
         var WebAPIURL = /** @class */ (function () {
             function WebAPIURL() {
             }
             WebAPIURL.authUrl = "http://localhost:89/WatchListAPI/Auth/Login";
-            WebAPIURL.getWatchData = "http://localhost:89/WatchListAPI/WatchData/Get?uid=";
+            WebAPIURL.getWatchDataUrl = "http://localhost:89/WatchListAPI/WatchData/Get";
             return WebAPIURL;
         }());
         ControlConstants.WebAPIURL = WebAPIURL;
+        var Value = /** @class */ (function () {
+            function Value() {
+            }
+            Value.MaxReview = 5;
+            return Value;
+        }());
+        ControlConstants.Value = Value;
     })(ControlConstants = WatchListConstants.ControlConstants || (WatchListConstants.ControlConstants = {}));
 })(WatchListConstants || (WatchListConstants = {}));
 //# sourceMappingURL=Constants.js.map
--- WatchList/WatchListUI/Constants.ts
+++ WatchList/WatchListUI/Constants.ts
@@ -7,10 +7,19 @@
             static readonly loginErrorMessage: string = "#LoginErrorMessage";
             static readonly dashboardWatchListDataTable: string = "#table_data";
             static readonly loader: string = "#loader";
+            static readonly signOut = "#sign_out";
+            static readonly watchDataTable = "#watchDataTable";
+            static readonly watchDataTableBody = "#watchDataTableBody";
+        }
+        export class LocalStorage{
+            static readonly tokenKey: string = "watchListSecureToken";
         }
         export class WebAPIURL{
             static readonly authUrl: string = "http://localhost:89/WatchListAPI/Auth/Login";
-            static readonly getWatchData: string = "http://localhost:89/WatchListAPI/WatchData/Get?uid=";
+            static readonly getWatchDataUrl: string = "http://localhost:89/WatchListAPI/WatchData/Get";
+        }
+        export class Value {
+            static readonly MaxReview: number = 5;
         }
     }
 }
